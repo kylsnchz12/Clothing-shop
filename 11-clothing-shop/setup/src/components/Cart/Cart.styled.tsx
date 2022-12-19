bf0c6e489d2524291f78f6f79d0ea0ac23cc16e1
Kyle <kyle_sanchez99@yahoo.com>
2022-12-19 16:57:23 +0800

    Created a checkout button

--- 11-clothing-shop/setup/src/components/Cart/Cart.styled.tsx
+++ 11-clothing-shop/setup/src/components/Cart/Cart.styled.tsx
@@ -12,3 +12,38 @@ export const ProductsWrapper = styled.div`
   grid-template-columns: repeat(2, auto);
   gap: 20px;
 `;
+
+export const ButtonWrap = styled.div`
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  grid-template-columns: repeat(2, auto);
+  gap: 20px;
+`;
+
+export const CheckOutButton = styled.button`
+  display: inline-block;
+  justify-content: center;
+  align-items: center;
+  top: 20px;
+  right: 20px;
+  width: 100px;
+  height: 30px;
+  background-color: #60c95d;
+  border: none;
+  border-radius: 3px;
+  color: white;
+  padding: 5px;
+  cursor: pointer;
+
+  :hover {
+    background-color: #25be65;
+    transition: 1s;
+  }
+  
+  p {
+    font-size: 20px;
+    margin: 0;
+    color: white;
+  }
+`;
--- 11-clothing-shop/setup/src/components/Cart/Cart.tsx
+++ 11-clothing-shop/setup/src/components/Cart/Cart.tsx
@@ -2,13 +2,16 @@ import { useContext } from 'react';
 import { ClothingShopContext } from '../../context/ShopContext';
 import { Product } from '../../models/Product';
 import { ProductCard } from '../ProductCard/ProductCard';
-import { ProductsWrapper, Title } from './Cart.styled';
+import { CheckOutButton, ProductsWrapper, Title, ButtonWrap } from './Cart.styled';
 
 export const Cart = () => {
   const { products, total } = useContext(ClothingShopContext);
   return (
     <>
-      <Title>Your cart total is {total}.00$</Title>
+      <ButtonWrap>
+        <Title>Your cart total is {total}.00$</Title>
+          <CheckOutButton>Check out</CheckOutButton>
+      </ButtonWrap>
       <ProductsWrapper>
         {products.map((product: Product, index) => (
           <ProductCard {...product} key={index} />
