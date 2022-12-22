aa811265802d78f875a6a6fbcf0580ec20132d3b
Kyle <kyle_sanchez99@yahoo.com>
2022-12-22 20:06:02 +0800

    Removing unnecessary functions

--- 11-clothing-shop/setup/src/CheckoutCard/CheckoutCard.tsx
+++ 11-clothing-shop/setup/src/CheckoutCard/CheckoutCard.tsx
@@ -1,25 +1,12 @@
 import {
-    AddButton,
     ItemWrap,
-    SubTitle,
-    TextContainer,
     TextWrap,
-    Title,
-    WishListButton,
     Wrapper,
 } from './CheckoutCard.styled';
 
 import { Product } from '../models';
-import { useContext, useEffect, useState } from 'react';
-import { ClothingShopContext } from '../context';
-import { BsFillGiftFill } from "react-icons/bs"
 
 export const CheckoutCard = ({ name, imageUrl, price, quantity }: Product) => {
-    const {products, addToCart, removeItem} = useContext(ClothingShopContext);
-    const [isInCart, setIsInCart] = useState(false);
-    const [isInWishList, setIsInWishList] = useState(false);
-    const {items, addToList, removeList} = useContext(ClothingShopContext);
-
     return (
         <ItemWrap>
             <Wrapper background={imageUrl}>
